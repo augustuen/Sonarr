8fc10b8fd7ab2cbf5eec7650032fa233a1ddca81
Lars Erik Helø <lars.erik.heloe@gmail.com>
2023-09-09 20:57:04 +0200

    [nonfunctional] Start of adding Porla torrent client.

---
+++ src/NzbDrone.Core/Download/Clients/Porla/PorlaError.cs
@@ -0,0 +1,8 @@
+namespace NzbDrone.Core.Download.Clients.Porla
+{
+    public class PorlaError
+    {
+        public string Message { get; set; }
+        public int Code { get; set; }
+    }
+}
---
+++ src/NzbDrone.Core/Download/Clients/Porla/PorlaPreferences.cs
@@ -0,0 +1,6 @@
+namespace NzbDrone.Core.Download.Clients.Porla
+{
+    public class PorlaPreferences
+    {
+    }
+}
